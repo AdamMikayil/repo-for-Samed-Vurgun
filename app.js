6cc77302b94162df7dd11cf485df60341e0cb9b6
Adam <mikayilov.adam@gmail.com>
2025-02-17 07:36:58 +0400

    push

---
+++ app.js
@@ -0,0 +1,83 @@
+let main_info_text = ""
+
+function translate_aze_title() {
+    document.getElementById('CAMEД').innerHTML = 'Səməd Vurğun';
+}
+
+function translate_ru_title() {
+    document.getElementById('CAMEД').innerHTML = 'Самед Вургун';
+}
+
+function translate_mainInfo_ru() {
+    document.getElementById('main_info').textContent =
+        'Саме́д Вургу́н (азерб. Səməd Vurğun; настоящее имя — Самед Юсиф оглы Веки́лов, азерб. Səməd Yusif oğlu Vəkilov;\n\
+    21 марта 1906, Юхары-Салахлы, Кавказское наместничество — 27 мая 1956, Баку) — азербайджанский советский поэт,\n\
+    драматург и общественный деятель.\n\
+\n\
+    Первый Народный поэт Азербайджанской ССР (1956), академик АН Азербайджанской ССР (1945). Лауреат двух Сталинских\n\
+    премий второй степени (1941, 1942). Член ВКП(б) с 1940 года.';
+}
+
+
+function translate_mainInfo_az() {
+    document.getElementById('main_info').textContent = 'Səməd Vurğun (azərb. Səməd Vurğun; əsl adı - Səməd Yusif oğlu Vəkilov, azərbaycanlı. Səməd Yusif oğlu Vəkilov;\n\
+ 21 mart 1906, Yuxarı-Salahlı, Qafqaz canişini - 27 may 1956, Bakı) — Azərbaycan sovet şairi,\n\
+ dramaturq və ictimai xadim.\n\
+\n\
+ Azərbaycan SSR ilk xalq şairi (1956), Azərbaycan SSR EA-nın akademiki (1945). İki Stalin laureatı\n\
+ ikinci dərəcəli mükafatlar (1941, 1942). 1940-cı ildən Ümumittifaq Kommunist Partiyasının (bolşeviklər) üzvü.'
+}
+
+function translate_poem_ru() {
+    document.getElementById('poem').textContent =
+        '«Комсомольская поэма» (1928—1956, 1-я часть — 1933)n\
+«Событие» (1932),\n\
+«Мурадхан» (1933),\n\
+«Хумар» (1933),\n\
+«Локбатан» (1933),\n\
+«Сельское утро» (1933),\n\
+«Скамья смерти» (1934),\n\
+«Горькие воспоминания» (1935),\n\
+«Двадцать шесть» (1935),\n\
+«Виселица» (1935),\n\
+«Погибшая любовь» (1935),\n\
+«Бунт» (1936),\n\
+«Слово о колхознице Басти» (1937),\n\
+«Бакинский дастан» или же «Дастан Баку» (1944)\n\
+и др.';
+}
+
+function translate_poem_az() {
+    document.getElementById('poem').textContent =
+        '"Komsomol poeması " (1928-1956, 1-ci hissə - 1933)\n\
+"Hadisə" (1932),\n\
+"Muradxan" (1933),\n\
+"Humar" (1933),\n\
+"Lökbatan" (1933),\n\
+"Ölkə səhəri" (1933),\n\
+"Ölüm skamyası" (1934),\n\
+"Acı xatirələr" (1935),\n\
+"İyirmi altı" (1935),\n\
+"Darağacları" (1935),\n\
+"Sona çatan sevgi" (1935)\n\
+"Üsyan" (1936),\n\
+"Kolxozçu Bəsti haqqında söz" (1937),\n\
+"Bakı Dastanı" və ya "Dastan Bakı" (1944)\n\
+və başqaları.';
+}
+
+function translate_plays_ru() {
+    document.getElementById('plays').textContent =
+        '«Вагиф» (1937) — драма о трагической судьбе Молла Панаха Вагифа.\n\
+«Ханлар» (1939) — пьеса, посвящённая жизни революционера Ханлара Сафаралиева.\n\
+«Фархад и Ширин» (1941) — стихотворная драма по мотивам поэмы Низами «Хосров и Ширин».\n\
+«Человек» (1945).';
+}
+
+function translate_plays_az() {
+    document.getElementById('plays').textContent =
+        '"Vaqif " (1937) Molla Pənah Vaqifin faciəvi taleyindən bəhs edən dramdır .\n\
+"Xanlar" (1939) - inqilabçı Xanlar Səfərəliyevin həyatına həsr olunmuş tamaşadır .\n\
+"Fərhad və Şirin " (1941) - Nizaminin " Xosrov və Şirin " poeması əsasında yazılmış mənzum dramdır .\n\
+"İnsan" (1945).';
+}
